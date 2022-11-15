516436228b92daf6de8c105b239ac7c859fcc9dd
Mateusz Macheta <mateusz.macheta@wp.pl>
2022-11-15 14:21:56 +0100

    new C# snippets folder

---
+++ C#/HTTP Post Request with binary body.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+string url = "<ENDPOINT URL>";
+string in_referralPDFLocation = @"<FILE PTH>";
+
+string username = "<USERNAME>";
+string password = "<PASSWORD>";
+
+string responseBody;
+int responseStatus = 0;
+
+Byte[] byteFile = File.ReadAllBytes(in_referralPDFLocation);
+
+using( HttpClient client = new HttpClient())
+{
+    ByteArrayContent content = new ByteArrayContent(byteFile);
+    content.Headers.Add("content-type", "application/pdf");
+    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic",
+        Convert.ToBase64String(Encoding.UTF8.GetBytes($"{username}:{password}")));
+    HttpResponseMessage response = client.PostAsync(url, content).Result;
+    responseStatus = (int)response.StatusCode;
+    response.EnsureSuccessStatusCode();
+    responseBody = response.Content.ReadAsStringAsync().Result;
+}
+
+
+Console.WriteLine(responseBody);
+Console.WriteLine(responseStatus);
